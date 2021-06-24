b3772b639f8f455c1163f12aa6d290c711bae194
farhadhsn <farhadhsn8@gmail.com>
2021-06-24 22:18:08 +0430

    route - blade

--- Laravel_LLL/LP/routes/web.php
+++ Laravel_LLL/LP/routes/web.php
@@ -18,12 +18,22 @@ Route::get('/', function () {
 });
 
 
-Route::get('/about',function (){
-    echo "<h1>jalase avval tamrin</h1>";
-});
 
 
-Route::get('/about/{yourage}/{yourname}',function ($age ,$name){
-    echo "<h2>سن شما '$age' میباشد </h2> ";
-    echo "      اسم شما$name  است ";
+Route::prefix('/about')->group(function (){
+    Route::get('',function (){
+        echo "<h1>jalase avval tamrin</h1>";
+    });
+
+
+    Route::get('/{yourage}/{yourname}',function ($age ,$name){
+        echo "<h2>سن شما '$age' میباشد </h2> ";
+        echo "      اسم شما$name  است ";
+    });
+
+    Route::get('/{yourage}',function ($age){
+        return view('age')->with('age',$age);
+    });
+
+
 });
