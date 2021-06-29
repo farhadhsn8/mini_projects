a18ab28f1cf7787c69270f73d78924bbfc2c3f2e
farhadhsn <farhadhsn8@gmail.com>
2021-06-29 20:48:11 +0430

    eloquent

--- Laravel_LLL/LP/app/Models/Flight.php
+++ Laravel_LLL/LP/app/Models/Flight.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Flight extends Model
 {
+    use SoftDeletes;
+    protected $table = 'flights';
+    protected  $fillable=['name'];
+//    protected $primaryKey = 'flight_id';
+//    protected $keyType = 'string';
+//    public $incrementing = false;
+//    public $timestamps = false;
+//    const CREATED_AT = 'creation_date';
+//    const UPDATED_AT = 'updated_date';
+//    protected $connection = 'sqlite';
+//    protected $attributes = [
+//        'delayed' => false,
+//    ];
+
     use HasFactory;
 }
--- Laravel_LLL/LP/routes/web.php
+++ Laravel_LLL/LP/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Flight;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -75,5 +76,117 @@ Route::prefix('/flights')->group(function (){
     });
 
 
+
 });
+
+
+
+Route::get('f0',function (){
+    $i=1;
+    foreach (Flight::all() as $flight) {
+        echo $i." - ".$flight->name."<br>";
+        $i++;
+    }
+});
+
+Route::get('f1',function (){
+    $i=1;
+    $flights = Flight::where('airline', 'hosseini')
+        ->orderBy('name')
+        ->take(2)
+        ->get();
+    foreach ($flights as $flight) {
+        echo $i." - ".$flight->name."<br>";
+        $i++;
+    }
+});
+
+
+Route::get('f2',function (){
+    Flight::chunk(10, function ($flights) {
+        $i=1;
+        foreach ($flights as $flight) {
+            echo $i." - ".$flight->name."<br>";
+            $i++;
+
+        }
+    });
+});
+
+
+Route::get('f3',function (){
+    $i=1;
+    $flight = Flight::find(1);
+
+    echo $i." - ".$flight->name."<br>";
+
+});
+
+
+Route::get('f4',function (){
+    $i=1;
+    $flight = Flight::findOrFail(8);
+    echo $i." - ".$flight->name."<br>";
+
+
+});
+
+
+//Route::get('f5',function (){
+//    $flight = Flight::firstOrCreate([
+//        'name' => 'London to Paris']
+//    );
+//    $i=1;
+//    $flight = Flight::findOrFail('name','London to Paris');
+//    echo $i." - ".$flight->name."<br>";
+//
+//});
+
+
+//$count = Flight::where('active', 1)->count();
+//
+//$max = Flight::where('active', 1)->max('price');
+
+
+Route::get('f6',function (){
+    $flight = new Flight;
+
+    $flight->name = 'farhad';
+    $flight->airline = 'hosseini';
+    $flight->information = 'farhad  -  hosseini';
+
+    $flight->save();
+
+
+});
+/*$flight = Flight::create([
+    'name' => 'London to Paris',
+]);*/
+
+
+Route::get('f7',function (){
+    $flight = Flight::where('airline', 'hosseini')
+        ->first();
+
+    $flight->name = 'mahdi';
+    $flight->airline = 'benvidi';
+
+
+    $flight->save();
+
+
+});
+/*
+Flight::where('active', 1)
+    ->where('destination', 'San Diego')
+    ->update(['delayed' => 1]);*/
+
+
+
+
+
+
+//$deletedRows = Flight::where('active', 0)->delete();
+
+
 
